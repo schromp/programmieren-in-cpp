a300b46d803b60e7698129002c7e4c9dfeb862d2
Lennart Koziollek <lennart.koziollek@stud.uni-due.de>
2022-10-21 14:13:04 +0200

    zs

--- ex02/task02/main.c
+++ ex02/task02/main.c
@@ -3,56 +3,20 @@
 void printsizes();
 
 /**
- * @brief 
- * 
- * @return int 
+ * @brief
+ *
+ * @return int
  */
 int main(void)
 {
     int cube1[3][2][4] = {{{1, 2, 3, 4}}, {{2, 3}, {4, 6, 8, 10}}, {{3, 4, 5, 6}, {6, 8, 10}}};
-
-    printf("char %lu\n", sizeof(cube1));
-
-    printf("Big Steps: \n");
-
-    printf("ptr=%d\n", &cube1[0][0][0]);
-    printf("ptr=%d\n", &cube1[1][0][0]);
-    printf("ptr=%d\n", &cube1[2][0][0]);
-
-    printf("Medium Steps: \n");
-
-    printf("ptr=%d\n", &cube1[0][0][0]);
-    printf("ptr=%d\n", &cube1[0][1][0]);
-    printf("ptr=%d\n", &cube1[0][2][0]);
-    printf("ptr=%d\n", &cube1[0][3][0]);
-
-    printf("Small: Steps: \n");
-
-    printf("First \n");
-    
-    printf("ptr=%d\n", &cube1[0][0][0]);
-    printf("ptr=%d\n", &cube1[0][0][1]);
-    printf("ptr=%d\n", &cube1[0][0][2]);
-    printf("ptr=%d\n", &cube1[0][0][3]);
-
-    printf("Second: \n");
-
-    printf("ptr=%d\n", &cube1[1][0][0]);
-    printf("ptr=%d\n", &cube1[1][0][1]);
-    printf("ptr=%d\n", &cube1[1][1][0]);
-    printf("ptr=%d\n", &cube1[1][1][1]);
-    printf("ptr=%d\n", &cube1[1][1][2]);
-    printf("ptr=%d\n", &cube1[1][1][3]);
-
-    printf("Third \n");
-
-    printf("ptr=%d\n", &cube1[2][0][0]);
-    printf("ptr=%d\n", &cube1[2][0][1]);
-    printf("ptr=%d\n", &cube1[2][0][2]);
-    printf("ptr=%d\n", &cube1[2][0][3]);
-    printf("ptr=%d\n", &cube1[2][1][0]);
-    printf("ptr=%d\n", &cube1[2][1][1]);
-    printf("ptr=%d\n", &cube1[2][1][2]);
+    int cube2[2][3][3] = {{{9, 8, 7}, {6}, {5, 4}}, {{3, 2}, {1, 2, 3}, {4}}};
 
     return 0;
+}
+
+int sum2nd(int *cube, int x, int y, int z){
+
+    
+
 }
